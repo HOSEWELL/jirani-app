646103e4311d28c70e6b6b388a4cf74c07dfb934
HOSEWELL <hosewellkaranja@gmail.com>
2024-11-18 21:18:44 +0300

    jhg

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -74,7 +74,7 @@ const Posts = () => {
     <div className="p-3 md:p-6">
       <h2 className="text-2xl md:text-3xl font-semibold mb-4 md:mb-6">Posts</h2>
       {posts.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
           {posts.map((post) => (
             <div
               key={post.id}
